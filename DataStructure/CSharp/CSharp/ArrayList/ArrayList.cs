61102cde9d8c25e27e226bd3fcc6d9e3445acfe5
leesh5000 <leesh50000@gmail.com>
2020-11-19 03:52:01 +0900

    Add CSharp, Java

---
+++ DataStructure/CSharp/CSharp/ArrayList/ArrayList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class ArrayList
+    {
+        public ArrayList()
+        {
+            LinkedList<int> a = new LinkedList<int>();
+
+            a.AddAfter(100);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp");
+        }
+    }
+}
