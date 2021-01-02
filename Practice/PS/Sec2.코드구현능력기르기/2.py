f766110db555ff33a93434a68b3e38d5b3357f18
leesh5000 <leesh50000@gmail.com>
2021-01-02 17:39:10 +0900

    update

--- Practice/PS/Sec2.코드구현능력기르기/2.py
+++ Practice/PS/Sec2.코드구현능력기르기/2.py
@@ -1,11 +1,27 @@
-with open('./PS/input.txt', 'r') as f:
-    t = int(f.readline())
-    for i in range(t):
-        n,s,e,k = map(int, f.readline().split())
-        lt = list(map(int, f.readline().split()))
+from filecmp import cmp
 
-        """ solution """
-        lt = lt[s-1:e]
-        lt.sort()
-        print("#%d %d" % (i+1,lt[k-1]))
+# in{}의 개수만큼 for문
+for i in range(1,6):
+    with open('./PS/in{}.txt'.format(i), 'r') as f1:
+        with open('./PS/my_out{}.txt'.format(i), 'w') as f2:
+            t = int(f1.readline())
+            for j in range(t):
+                n,s,e,k = map(int, f1.readline().split())
+                lt = list(map(int, f1.readline().split()))
 
+                """ solution """
+                lt = lt[s-1:e]
+                lt.sort()
+                print("#%d %d" % (j+1,lt[k-1]))
+
+                """ file write """
+                f2.write("#%d %d" % (j+1,lt[k-1]))
+                if j+1!=t:
+                    f2.write('\n')
+
+
+            """ file comparison """
+            if cmp('./PS/my_out{}.txt'.format(i), './PS/out{}.txt'.format(i)):
+                print('Test #%d: right' % (i))
+            else:
+                print('Test #%d: wrong' % (i))
--- Practice/PS/Sec2.코드구현능력기르기/tempCodeRunnerFile.py
+++ Practice/PS/Sec2.코드구현능력기르기/tempCodeRunnerFile.py
@@ -1,7 +1,18 @@
-with open('./PS/input.txt', 'r') as f:
-#     t = int(f.readline())
-#     for i in range(t):
-#         l = f.readline().split()
-#         print(l)
-#         n,s,e,k = list(map(int, l))
-#         print(n,s,e,k)
+from filecmp import cmp
+
+# for i in range(2):
+#     with open('./PS/test_input{}.txt'.format(i+1), 'r') as f:
+#         with open('./PS/my_output{}.txt'.format(i+1), 'w') as fw:
+#             t = int(f.readline())
+#             for j in range(t):
+#                 a, b = map(int, f.readline().split())
+#                 print("#%d %d" %(j+1, a+b))
+            
+#                 fw.write('#%d %d' %(j+1, a+b))
+#                 fw.write('\n')
+
+#             # 파일비교알고리즘
+#             if cmp('./PS/my_output{}.txt'.format(i+1), './PS/test_output{}.txt'.format(i+1)):
+#                 print('Test #%d: right' % (i+1))
+#             else:
+#                 print('Test #%d: wrong' % (i+1))
