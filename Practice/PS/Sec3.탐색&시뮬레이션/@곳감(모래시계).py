288e73d6761111d86a2a05fbe937d7134e731df7
leesh5000 <leesh50000@gmail.com>
2021-01-13 22:55:38 +0900

    update

--- Practice/PS/Sec3.탐색&시뮬레이션/@곳감(모래시계).py
+++ Practice/PS/Sec3.탐색&시뮬레이션/@곳감(모래시계).py
@@ -1,20 +1,18 @@
 import sys
-for i in range(1,2):
-    sys.stdin = open("./PS/source/in{}.txt".format(i), 'r')
+for i in range(1,6):
+    sys.stdin = open('./PS/source/in{}.txt'.format(i), 'r')
+
     n = int(input())
-    a = [list(map(int, input().split())) for i in range(n)]
+    a = [list(map(int, input().split())) for _ in range(n)]
     m = int(input())
     for i in range(m):
         r, d, c = map(int, input().split())
         if d==0:
-            temp = []
-            for j in range(n):
-                temp[j] = a[r][(j+3)%n]
+            for _ in range(c):
+                a[r-1].append(a[r-1].pop(0))
         else:
-            temp = []
-            for j in range(n):
-                temp[j] = a[r][(j-3)%n]
-            a[r] = temp
+            for _ in range(c):
+                a[r-1].insert(0, a[r-1].pop())
     res = 0
     s = 0
     e = n-1
